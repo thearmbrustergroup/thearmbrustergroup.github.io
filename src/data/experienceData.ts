b463449ce13e1ece4ad2adf8505f827cf57bf086
John Armbruster <jaboyak@gmail.com>
2022-12-20 16:19:03 -0700

    fixed bad img links and updated experience

--- src/data/experienceData.ts
+++ src/data/experienceData.ts
@@ -1,42 +1,107 @@
 export default [
   {
-    businessName: 'Test Drive Buddy',
-    imgLink: 'images/biggertext.png',
-    startDate: 'September 2016',
-    endDate: 'Current',
-    resOne:
-      'Built production app using MEAN Stack (MongoDB, ExpressJS, AngularJS, and NodeJS)',
-    resTwo: 'Fixed bugs on active iteration as necessary',
-    resThree: 'Assisted with building client websites as necessary',
+    businessName: "Carvana",
+    imgLink:
+      "https://media.licdn.com/dms/image/C4E0BAQEberYZzSqRyw/company-logo_100_100/0/1643725649767?e=1679529600&v=beta&t=U0wtuz70_cl5z_rVxc7MSwdR9p74LJRu4Pq6f7Z_pEo",
+    startDate: "Jun 2021",
+    endDate: "Dec 2022",
+    description: [
+      "Built and maintain two applications that rely on external services that run on either GraphQL or REST endpoints.",
+      "Work on tickets for back-end work in NodeJS and C# / .NET and .NET Core.",
+      "All front ends are done in React and Typescript.",
+    ],
+    jobTitle: "Engineer II",
   },
   {
-    businessName: 'Inhouse Real Estate Marketing',
-    imgLink: 'images/inhouse-logo.svg',
-    startDate: 'Jun 2016',
-    endDate: 'October 2016',
-    resOne: 'Built inhouse app using Angular 1.x',
-    resTwo: 'Fixed bugs on active iteration as necessary',
-    resThree: 'Assisted with building client websites as necessary',
+    businessName: "Opendock",
+    imgLink:
+      "https://media.licdn.com/dms/image/C4D0BAQGmEqdyc2xL6Q/company-logo_100_100/0/1647890879962?e=1679529600&v=beta&t=vtQcxEpcLy-V5lxRR_9zvAnU0XKhRDwl3Eyy0Ac0d6Q",
+    startDate: "Feb 2021",
+    endDate: "May 2021",
+    description: [
+      "Build microservices in Laravel",
+      "Build custom client UIs with VueJS",
+      "Update and refresh previously built internal tools",
+    ],
+    jobTitle: "Software Engineer",
   },
   {
-    businessName: 'DevMountain',
-    imgLink: 'images/devmtnlogo1.png',
-    startDate: 'Jan 2016',
-    endDate: 'May 2016',
-    resOne: 'Fix bugs in the in-house apps when requested.',
+    businessName: "Blockparty Group",
+    imgLink:
+      "https://scontent.fphx1-1.fna.fbcdn.net/v/t39.30808-6/306789354_522812526515274_7948641015628680316_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=FzT8tHh1FRUAX-j4FrV&_nc_ht=scontent.fphx1-1.fna&oh=00_AfB6bXTMZC-KbOhrWkITUi5GLnK-OsDsNt-ZqXjqJqq17Q&oe=63A64365",
+    startDate: "Feb 2020",
+    endDate: "Jan 2021",
+    description: [
+      "Manage project team responsible for development of React Native App using GraphQL and Apollo. ",
+      "Full stack development includes administrator panel, custom API, and cross-platform mobile app for end users.",
+    ],
+    jobTitle: "Lead Software Engineer",
+  },
+  {
+    businessName: "Galvanize",
+    imgLink:
+      "https://media.licdn.com/dms/image/C560BAQFKNxOZ4X0g8Q/company-logo_100_100/0/1670610916338?e=1679529600&v=beta&t=tIwKnJ7ryP3R0xS4L7hH7qshgw9dr4a95598mSO0kx8",
+    startDate: "Jan 2017",
+    endDate: "Aug 2020",
+    description: [
+      "Lead 24-week software engineering courses",
+      "Managed instructors and led bi-weekly one on ones",
+      "Rewrote the React curriculum to be applicable to non-enterprise students",
+      "Conducted daily lesson planning and built most of the templates for students to use",
+    ],
+    jobTitle:
+      "Lead Web Development Instructor / Principal Full Stack Developer",
+  },
+  {
+    businessName: "Test Drive Buddy",
+    imgLink: "images/biggertext.png",
+    startDate: "Sept 2016",
+    endDate: "Jan 2019",
+    description: [
+      "Built production app using MEAN Stack (MongoDB, ExpressJS, AngularJS, and NodeJS)",
+      "Fixed bugs on active iteration as necessary",
+      "Assisted with building client websites as necessary",
+    ],
+    jobTitle: "Founder/CTO",
+  },
+  {
+    businessName: "Inhouse Real Estate Marketing",
+    imgLink: "images/inhouse-logo.svg",
+    startDate: "Jun 2016",
+    endDate: "Oct 2016",
+    description: [
+      "Built inhouse app using Angular 1.x",
+      "Fixed bugs on active iteration as necessary",
+      "Assisted with building client websites as necessary",
+    ],
+    jobTitle: "Lead Software Engineer",
+  },
+  {
+    businessName: "DevMountain",
+    imgLink: "images/devmtnlogo1.png",
+    startDate: "Jan 2016",
+    endDate: "May 2016",
+    resOne: "Fix bugs in the in-house apps when requested.",
     resTwo:
-      'Conducted daily group meetings for 6 students to review concepts and assigned the work from the previous day.',
-    resThree: 'Assist students with daily projects.',
+      "Conducted daily group meetings for 6 students to review concepts and assigned the work from the previous day.",
+    resThree: "Assist students with daily projects.",
+    description: [
+      "Fix bugs in the in-house apps when requested.",
+      "Conducted daily group meetings for 6 students to review concepts and assigned the work from the previous day.",
+      "Assist students with daily projects.",
+    ],
+    jobTitle: "Lead Software Engineer",
   },
   {
-    businessName: 'Keller Williams Southern Arizona',
-    imgLink: 'images/kwsalogo.jpg',
-    startDate: 'Jan 2013',
-    endDate: 'Jan 2016',
-    resOne: 'Designed responsive websites for clients with the Wordpress CRM',
-    resTwo:
-      'Taught weekly beginner HTML/CSS, Social Media/SEO, and business systems classes',
-    resThree:
-      'Consulted for large and small real estate teams to improve online presence and business efficiency',
+    businessName: "Keller Williams Southern Arizona",
+    imgLink: "images/kwsalogo.jpg",
+    startDate: "Jan 2013",
+    endDate: "Jan 2016",
+    description: [
+      "Designed responsive websites for clients with the Wordpress CRM",
+      "Taught weekly beginner HTML/CSS, Social Media/SEO, and business systems classes",
+      "Consulted for large and small real estate teams to improve online presence and business efficiency",
+    ],
+    jobTitle: "Freelance",
   },
-]
+];
