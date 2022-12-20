6ff0e2c1939ae0053e378b418b279b8bcf2e5ac8
johnazre <jaboyak@gmail.com>
2022-12-20 05:59:16 -0700

    initial commit

---
+++ src/data/experienceData.ts
@@ -0,0 +1,42 @@
+export default [
+  {
+    businessName: 'Test Drive Buddy',
+    imgLink: 'images/biggertext.png',
+    startDate: 'September 2016',
+    endDate: 'Current',
+    resOne:
+      'Built production app using MEAN Stack (MongoDB, ExpressJS, AngularJS, and NodeJS)',
+    resTwo: 'Fixed bugs on active iteration as necessary',
+    resThree: 'Assisted with building client websites as necessary',
+  },
+  {
+    businessName: 'Inhouse Real Estate Marketing',
+    imgLink: 'images/inhouse-logo.svg',
+    startDate: 'Jun 2016',
+    endDate: 'October 2016',
+    resOne: 'Built inhouse app using Angular 1.x',
+    resTwo: 'Fixed bugs on active iteration as necessary',
+    resThree: 'Assisted with building client websites as necessary',
+  },
+  {
+    businessName: 'DevMountain',
+    imgLink: 'images/devmtnlogo1.png',
+    startDate: 'Jan 2016',
+    endDate: 'May 2016',
+    resOne: 'Fix bugs in the in-house apps when requested.',
+    resTwo:
+      'Conducted daily group meetings for 6 students to review concepts and assigned the work from the previous day.',
+    resThree: 'Assist students with daily projects.',
+  },
+  {
+    businessName: 'Keller Williams Southern Arizona',
+    imgLink: 'images/kwsalogo.jpg',
+    startDate: 'Jan 2013',
+    endDate: 'Jan 2016',
+    resOne: 'Designed responsive websites for clients with the Wordpress CRM',
+    resTwo:
+      'Taught weekly beginner HTML/CSS, Social Media/SEO, and business systems classes',
+    resThree:
+      'Consulted for large and small real estate teams to improve online presence and business efficiency',
+  },
+]
